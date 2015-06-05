cab944eef093c6591bea3f80d3d479c93ea76ecd
Stud <vct.studer@gmail.com>
2015-06-05 11:48:18 +0200

    Breaking the ground

---
+++ Assets/Scripts/GOFactoryMachine.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GOF
+{
+    public class GOFactoryMachine
+    {
+        private string modelName;
+        public string ModelName
+        {
+            set { modelName = value; }
+        }
+
+        private GameObject prefab;
+        public GameObject Prefab
+        {
+            set { prefab = value; }
+        }
+
+        private List<GameObject> inUse;
+        private List<GameObject> waiting;
+
+        private Vector3 defaultPos;
+        public Vector3 DefaultPos
+        { set { defaultPos = value; } }
+
+        private float inactivityTimeBeforeDestroy;
+        public float InactivityTimeBeforeDestroy
+        { set { inactivityTimeBeforeDestroy = value; } }
+
+        private float activityTimeBeforeDestroy;
+        public float ActivityTimeBeforeDestroy
+        { set { activityTimeBeforeDestroy = value; } }
+
+        private void Start()
+        {
+            inactivityTimeBeforeDestroy = 0;
+            defaultPos = new Vector3(0, 0, 0);
+        }
+
+        public GOFactoryMachine()
+        {
+            inUse = new List<GameObject>();
+            waiting = new List<GameObject>();
+        }
+
+        public GameObject createModel(string id)
+        {
+            return createModel(id, defaultPos);
+        }
+
+        public GameObject createModel(string id, Vector3 position)
+        {
+            GameObject model = null;
+            if (waiting.Count > 0)
+            {
+                model = waiting[0];
+                waiting.Remove(model);
+                model.SetActive(true);
+                model.transform.position = position;
+                model.GetComponent<GOFactoryTracker>().Active = true;
+            }
+            else
+            {
+                if (prefab != null)
+                    model = GameObject.Instantiate(prefab);
+                else
+                {
+                    GameObject obj = Resources.Load(modelName) as GameObject;
+                    if(obj != null)
+                        model = GameObject.Instantiate(obj);
+                    else
+                        Debug.LogError("[GOF]Couldn't find any prefab named " + modelName + ". Returning null.");
+                }
+
+                model.transform.position = position;
+                if (modelName != "")
+                    model.name = modelName;
+
+                GOFactoryTracker tracker = model.AddComponent<GOFactoryTracker>();
+                tracker.Id = id;
+                tracker.Machine = this;
+                tracker.Active = true;
+                tracker.InactivityTimeBeforeDestroy = inactivityTimeBeforeDestroy;
+            }
+            inUse.Add(model);
+            return model;
+        }
+
+        public void putAway(GameObject obj)
+        {
+            var creepInList = inUse.Find(c => c.GetComponent<GOFactoryTracker>().Id == obj.GetComponent<GOFactoryTracker>().Id);
+            inUse.Remove(creepInList);
+            waiting.Add(creepInList);
+            if (inactivityTimeBeforeDestroy > 0)
+                StartCoroutine(destroyCoroutine());
+        }
+
+        public void remove(GameObject obj)
+        {
+            if (waiting.Contains(obj))
+                waiting.Remove(obj);
+            if (inUse.Contains(obj))
+                inUse.Remove(obj);
+            GameObject.Destroy(obj);
+        }
+
+        IEnumerator destroyCoroutine()
+        {
+            float coroutineStartTime = Time.time;
+            while (!isActive)
+            {
+                if (Time.time - coroutineStartTime > inactivityTimeBeforeDestroy)
+                {
+                    machine.remove(gameObject);
+                    break;
+                }
+                yield return new WaitForSeconds(inactivityTimeBeforeDestroy);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GOFactoryTracker.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GOF
+{
+    public class GOFactoryTracker : MonoBehaviour
+    {
+        public string Id;
+
+        private float inactivityTimeBeforeDestroy;
+        public float InactivityTimeBeforeDestroy
+        {
+            set { inactivityTimeBeforeDestroy = value; }
+        }
+
+        protected bool isActive;
+        public bool Active
+        {
+            get { return isActive; }
+            set
+            {
+                isActive = value;
+                activate(isActive);
+            }
+        }
+
+        void Start()
+        {
+
+        }
+
+        protected GOFactoryMachine machine;
+        public GOFactoryMachine Machine
+        {
+            set { machine = value; }
+        }
+
+        protected virtual void activate(bool b)
+        {
+            hide(b);
+        }
+
+        protected void hide(bool b)
+        {
+            gameObject.SetActive(b);
+            if (!b)
+                transform.position = new Vector3(0, 1000, 0);
+        }
+    }
+}
